1fa757bc17c5467e94695dee86a343951be809b0
cooljasonmelton <cooljasonmelton>
2020-10-11 21:32:08 -0500

    working on teacher side teacher and courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,25 @@
 class CoursesController < ApplicationController
+    def index
+        # sort by teacher last name
+        @courses = Course.all
+    end 
+
+    def show
+        
+    end 
+
+    def new
+    end 
+
+    def create
+    end 
+
+    def edit
+    end 
+
+    def update 
+    end
+
+    def destroy
+    end 
 end
